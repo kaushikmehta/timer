b09a8580e9f83ed709fb1a7b03c92199db4c78cf
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-06-30 19:12:11 +0000

    fixed edge case checks

--- timer1.js
+++ timer1.js
@@ -2,7 +2,8 @@ const args = process.argv.splice(2);
 
 if (args.length >= 1) {
   for (let time of args) {
-    if (typeof time * 2  !== 'NaN' && time >= 0) {
+    console.log(!isNaN(time));
+    if (!isNaN(time) && time >= 0) {
       time = Number(time);
       setTimeout(() => process.stdout.write('\x07'), time * 1000);
     }
